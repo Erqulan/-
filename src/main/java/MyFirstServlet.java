e6412c4cffbba500dadfcca91f1902ff095c4441
Erqulan <@erhulan66.com>
2025-02-06 12:55:01 +0500

    Initial commit

---
+++ src/main/java/MyFirstServlet.java
@@ -0,0 +1,20 @@
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "MyFirstServlet", value = "/hello")
+public class MyFirstServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Salem, Tomcat!</h1>");
+        out.println("</body></html>");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        <h1>MyWebApp!</h1>
+        <p> <a href="hello">/hello</a>.</p>
+    </body>
+</html>
